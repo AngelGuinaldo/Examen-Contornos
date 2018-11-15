cf5b4aa27cad0b981ff3a8bd366d138b83a315bc
aguinaldogonzalez <aguinaldogonzalez@nautilus4.danielcastelao.org>
2018-11-15 09:00:33 +0100

    Commit inicial A

---
+++ src/excod1av/ClaseRamaMaster.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package excod1av;
+
+/**
+ *
+ * @author aguinaldogonzalez
+ */
+public class ClaseRamaMaster {
+    // Commit Inicial A con clase nueva en la rama master
+    
+}
